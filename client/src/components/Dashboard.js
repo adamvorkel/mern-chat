acbb030d74ad1ea5d648ef1fdc652ef660b85b89
Adam Vorkel <adamvorkel@gmail.com>
2020-09-09 22:01:35 +0200

    added sidebar component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Sidebar from './Sidebar';
+
 const Dashboard = ({ id }) => {
     return (
         <div>
+            <Sidebar id={id} />
             <h1>Dashboard</h1>
             {id}
         </div>
---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = ({ id }) => {
+    return(
+        <div>
+            <h1>Sidebar</h1>
+            {id}
+        </div>
+    );
+};
+
+export default Sidebar;
